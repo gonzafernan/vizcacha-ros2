a878d1e37f6146f69889a66c71e84163e755f224
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2024-09-30 23:16:25 +0200

    Add simulation and odometry system using Gazebo

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Environment setup following [Setup ROS 2 with VSCode and Docker](https://docs.ro
 
 ## URDF robot description
 URDF setup following [Setting Up The URDF](https://docs.nav2.org/setup_guides/urdf/setup_urdf.html)
+
+## Robot odometry in ROS 2
+Odometry setup following [Setting Up Odometry](https://docs.nav2.org/setup_guides/odom/setup_odom.html)
+
+## Development with Docker
+The main resource used for the Docker image development was [An Updated Guide to Docker and ROS 2](https://roboticseabass.com/2023/07/09/updated-guide-docker-and-ros2/).
+
+Use of GUIs possible following [Using GUIs with Docker](https://wiki.ros.org/docker/Tutorials/GUI)
--- src/vizcc_description/launch/display.launch.py
+++ src/vizcc_description/launch/display.launch.py
@@ -47,6 +47,17 @@ def generate_launch_description():
         output="screen",
     )
 
+    robot_localization_node = launch_ros.actions.Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/ekf.yaml"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
+    )
+
     return launch.LaunchDescription(
         [
             launch.actions.DeclareLaunchArgument(
@@ -70,9 +81,15 @@ def generate_launch_description():
                 ],
                 output="screen",
             ),
+            launch.actions.DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="True",
+                description="Flag to enable use_sim_time",
+            ),
             joint_state_publisher_node,
             robot_state_publisher_node,
             spawn_entity,
+            robot_localization_node,
             rviz_node,
         ]
     )
