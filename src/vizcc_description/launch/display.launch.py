587cc248aa4dde81a76feebfa98405cc215732d5
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2024-09-30 22:34:23 +0200

    Modify launch file to spawn the robot in Gazebo

--- src/vizcc_description/launch/display.launch.py
+++ src/vizcc_description/launch/display.launch.py
@@ -1,10 +1,7 @@
 """
-Launches a robot publisher node in ROS 2 that uses our URDF to publish the
-transforms for our robot.
-In addition, the launch file also automatically launches RVIZ so we can
-visualize our robot as defined by the URDF.
+Spawn Vizcacha robot in Gazebo.
 
-See https://docs.nav2.org/setup_guides/urdf/setup_urdf.html
+See https://docs.nav2.org/setup_guides/odom/setup_odom.html
 """
 
 import launch
@@ -14,46 +11,68 @@ import os
 
 PACKAGE_NAME = "vizcc_description"
 
+
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package=PACKAGE_NAME).find(PACKAGE_NAME)
+    pkg_share = launch_ros.substitutions.FindPackageShare(package=PACKAGE_NAME).find(
+        PACKAGE_NAME
+    )
     default_model_path = os.path.join(pkg_share, f"src/description/{PACKAGE_NAME}.urdf")
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/urdf_config.rviz")
 
     robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {"robot_description": Command(["xacro ", LaunchConfiguration("model")])}
+        ],
     )
     joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', default_model_path])}],
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        arguments=[default_model_path],
     )
     rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
     )
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        rviz_node
-    ])
+    spawn_entity = launch_ros.actions.Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "sam_bot", "-topic", "robot_description"],
+        output="screen",
+    )
+
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(
+                name="model",
+                default_value=default_model_path,
+                description="Absolute path to robot urdf file",
+            ),
+            launch.actions.DeclareLaunchArgument(
+                name="rvizconfig",
+                default_value=default_rviz_config_path,
+                description="Absolute path to rviz config file",
+            ),
+            launch.actions.ExecuteProcess(
+                cmd=[
+                    "gazebo",
+                    "--verbose",
+                    "-s",
+                    "libgazebo_ros_init.so",
+                    "-s",
+                    "libgazebo_ros_factory.so",
+                ],
+                output="screen",
+            ),
+            joint_state_publisher_node,
+            robot_state_publisher_node,
+            spawn_entity,
+            rviz_node,
+        ]
+    )
