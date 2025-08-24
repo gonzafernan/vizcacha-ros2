0fb150673188ef89e19fbd424a4867e0d9dcf0ca
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-08-24 16:36:06 +0200

    feat: update gazebo config and add launch file

---
+++ src/vizcc_description/launch/gazebo.launch.py
@@ -0,0 +1,73 @@
+"""
+Spawn Vizcacha robot in Gazebo simulator.
+
+See https://docs.nav2.org/setup_guides/odom/setup_odom.html
+"""
+
+import os
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+PACKAGE_NAME = "vizcc_description"
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory(PACKAGE_NAME)
+
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(pkg_share, "urdf", f"{PACKAGE_NAME}.urdf.xacro"),
+        description="Absolute path to robot URDF file",
+    )
+
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]), value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+    )
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH", value=[str(Path(pkg_share).parent.resolve())]
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("ros_gz_sim"), "launch"),
+                "/gz_sim.launch.py",
+            ],
+        ),
+        launch_arguments=[("gz_args", [" -v 4", " -r", " empty.sdf"])],
+    )
+
+    gazebo_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description", "-name", "vizcacha_robot"],
+    )
+
+    return LaunchDescription(
+        [
+            model_arg,
+            robot_state_publisher_node,
+            gazebo_resource_path,
+            gazebo,
+            gazebo_spawn_entity,
+        ]
+    )
